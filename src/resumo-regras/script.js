d2853b31053793eccba782d964e4d88130dc689b
Pedro Costa <pedro4costa@outlook.com>
2024-11-12 12:19:52 -0300

    Mensagem do commit explicando as alterações

--- src/resumo-regras/script.js
+++ src/resumo-regras/script.js
@@ -2,6 +2,10 @@ function mostrarTexto () {
     let idBotao = event.target.getAttribute('id');
     console.log(idBotao);
     let textos = document.querySelectorAll(".descricao");
-    textos[idBotao].style.display = "none";
-    
+    textos.forEach((elemento) => {
+        elemento.style.display = "none";
+    });
+    textos[idBotao].style.display = "block";
+    let rodape = document.querySelector(".rodape");
+    rodape.style.position = "relative";   
 }
