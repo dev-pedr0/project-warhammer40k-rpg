a63fe4631773e9cf3328e851a3a69381354c9d88
Pedro Costa <pedro4costa@outlook.com>
2024-11-16 22:17:04 -0300

    Adição do ícone de usuário e opções em todas as páginas

--- src/resumo-regras/script.js
+++ src/resumo-regras/script.js
@@ -1,3 +1,4 @@
+//Controle das páginas de regras
 function mostrarTexto () {
     let idBotao = event.target.getAttribute('id');
     console.log(idBotao);
@@ -8,4 +9,22 @@ function mostrarTexto () {
     textos[idBotao].style.display = "block";
     let rodape = document.querySelector(".rodape");
     rodape.style.position = "relative";
+}
+
+//Controle das opções do usuário
+const usuario =document.querySelector(".usuario");
+const usuarioOpcoes = document.querySelector(".usuario-opcoes");
+
+usuario.addEventListener("click", abrirOpcoesUsuario);
+console.log(usuarioOpcoes.style);
+
+document.addEventListener("click", (evento) => {
+    if (!usuarioOpcoes.contains(evento.target) && !usuario.contains(evento.target)) {
+        usuarioOpcoes.classList.remove("toogle-block");
+        console.log("oi");
+    }
+});
+
+function abrirOpcoesUsuario() {
+    usuarioOpcoes.classList.add("toogle-block");
 }
